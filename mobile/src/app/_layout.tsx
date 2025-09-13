e31ff1e368e433ad62d1d2cbdb9be50fc7de4233
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-09-13 17:16:29 +0500

    [mobile] react-reusables setup

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,5 +1,27 @@
-import { Stack } from "expo-router";
+import { ThemeProvider } from '@react-navigation/native'
+import { PortalHost } from '@rn-primitives/portal'
+import { SplashScreen, Stack } from "expo-router"
+import { StatusBar } from 'expo-status-bar'
+import { useEffect } from 'react'
+import { NAV_THEME } from '@/shared/lib/theme'
+import { useColorScheme } from 'react-native'
+import '../global.css'
 
-export default function RootLayout() {
-  return <Stack />;
+const RootLayout = () => {
+  const colorScheme = useColorScheme()
+
+  return (
+    <ThemeProvider value={NAV_THEME[ colorScheme ?? 'dark' ]}>
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+      <Stack screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <PortalHost />
+    </ThemeProvider>
+  )
 }
+
+export default RootLayout
